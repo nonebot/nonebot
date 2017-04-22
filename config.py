ab2d5f500cb20e69003b7654fe573a564f50ca5a
waylon <waylon.act@gmail.com>
2017-04-22 09:15:08 +0800

    修正配置项

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 config = {
     'fallback_command': 'natural_language.process',
     'fallback_command_after_nl_processors': 'ai.tuling123',
-    'command_start_flags': (''),  #('/', '／', '来，', '来,'),  # Add '' (empty string) here to allow commands without start flags
+    'command_start_flags': ('', '/', '／'),  # Add '' (empty string) here to allow commands without start flags
     'command_name_separators': ('->', '::', '/'),  # Regex
-    'command_args_start_flags': (' '), #('，', '：', ',', ', ', ':', ': '),  # Regex
-    'command_args_separators': (' '), #('，', ','),  # Regex
+    'command_args_start_flags': ('，', '：', ',', ', ', ':', ': ','[\\s]'),  # Regex
+    'command_args_separators': ('，', ',','[\\s]'),  # Regex
 
     'message_sources': [
         {
