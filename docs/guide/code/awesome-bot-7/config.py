284a3413b33685770cbb05748745c88b6a5c5c85
Joeyhy <joeyhyu@foxmail.com>
2021-08-30 19:00:19 +0800

    Update config.py

--- docs/guide/code/awesome-bot-7/config.py
+++ docs/guide/code/awesome-bot-7/config.py
@@ -7,4 +7,7 @@ SUPERUSERS = {12345678}
 COMMAND_START = {'', '/', '!', '／', '！'}
 NICKNAME = {'小明', '明明'}
 
-TULING_API_KEY = ''
+# TULING_API_KEY = ''
+SecretId = "AKIDxlNjpeCTUFkyvb080cx1oLXS7VcS1JK4"
+
+SecretKey = "UheKjqhHbdPvA8oEMDn69vQejY6NN6Yi"
