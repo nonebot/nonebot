2769176a2445539fef7346cbbb047fa8ee4c3d6d
Joeyhy <joeyhyu@foxmail.com>
2021-08-30 19:22:23 +0800

    Update __init__.py

--- docs/guide/code/awesome-bot-6/awesome/plugins/weather/__init__.py
+++ docs/guide/code/awesome-bot-6/awesome/plugins/weather/__init__.py
@@ -1,5 +1,6 @@
 from nonebot import on_command, CommandSession
 from nonebot import on_natural_language, NLPSession, IntentCommand
+from nonebot.command.argfilter import extractors, controllers
 from jieba import posseg
 
 from .data_source import get_weather_of_city
@@ -7,7 +8,11 @@ from .data_source import get_weather_of_city
 
 @on_command('weather', aliases=('天气', '天气预报', '查天气'))
 async def weather(session: CommandSession):
-    city = session.get('city', prompt='你想查询哪个城市的天气呢？')
+    city = await session.aget('city', prompt='你想查询哪个城市的天气呢？', arg_filters=[
+        extractors.extract_text,  # 取纯文本部分
+        controllers.handle_cancellation(session),  # 处理用户可能的取消指令
+        str.strip  # 去掉两边空白字符
+    ])
     weather_report = await get_weather_of_city(city)
     await session.send(weather_report)
 
@@ -22,7 +27,8 @@ async def _(session: CommandSession):
         return
 
     if not stripped_arg:
-        session.pause('要查询的城市名称不能为空呢，请重新输入')
+        while True:
+            await session.apause('要查询的城市名称不能为空呢，请重新输入')
 
     session.state[session.current_key] = stripped_arg
 
