811c1b4af4c29303873801552aad538ad0f0596e
Joeyhy <joeyhyu@foxmail.com>
2021-09-07 09:11:17 +0800

    Update weather.py

--- docs/guide/code/awesome-bot-2/awesome/plugins/weather.py
+++ docs/guide/code/awesome-bot-2/awesome/plugins/weather.py
@@ -7,14 +7,7 @@ from nonebot.command.argfilter import extractors, controllers
 @on_command('weather', aliases=('天气', '天气预报', '查天气'))
 async def weather(session: CommandSession):
     # 从会话状态（session.state）中获取城市名称（city），如果当前不存在，则询问用户
-    city = await session.aget(
-        'city',
-        prompt='你想查询哪个城市的天气呢？',
-        arg_filters=[
-            extractors.extract_text,  # 取纯文本部分
-            controllers.handle_cancellation(session),  # 处理用户可能的取消指令
-            str.strip  # 去掉两边空白字符
-        ])
+    city = session.get('city', prompt='你想查询哪个城市的天气呢？')
     # 获取城市的天气预报
     weather_report = await get_weather_of_city(city)
     # 向用户发送天气预报
@@ -40,7 +33,7 @@ async def _(session: CommandSession):
         # 用户没有发送有效的城市名称（而是发送了空白字符），则提示重新输入
         # 这里 session.pause() 将会发送消息并暂停当前会话（该行后面的代码不会被运行）
         while True:
-            await session.apause('要查询的城市名称不能为空呢，请重新输入')
+            await session.pause('要查询的城市名称不能为空呢，请重新输入')
 
     # 如果当前正在向用户询问更多信息（例如本例中的要查询的城市），且用户输入有效，则放入会话状态
     session.state[session.current_key] = stripped_arg
