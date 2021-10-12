9303e8b7d43d5f4df4f6b1791fa8329d9149b62e
cleoold <cleoold@gmail.com>
2021-10-12 11:53:08 -0400

    test: change dir structure

--- testing/external/test_external.py
+++ testing/external/test_external.py
@@ -1,6 +1,6 @@
 from testing.external.common.client import run_test_as_subprocess
 
 
-class TestBasicsWork:
+class TestCommand:
     def test_basics_work(self):
         assert run_test_as_subprocess('test_command/test_basics_work') == 0
