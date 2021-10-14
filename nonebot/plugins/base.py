a53118de422f2d13267f06e01212ff416bd9ce3d
cleoold <cleoold@gmail.com>
2021-10-13 23:56:05 -0400

    doc: explain legacy permission
    also fix builtin plugin

--- nonebot/plugins/base.py
+++ nonebot/plugins/base.py
@@ -1,4 +1,4 @@
-from nonebot import on_command, CommandSession, permission as perm
+from nonebot import on_command, CommandSession
 from nonebot.message import unescape
 
 
@@ -7,7 +7,7 @@ async def echo(session: CommandSession):
     await session.send(session.state.get('message') or session.current_arg)
 
 
-@on_command('say', permission=perm.SUPERUSER)
+@on_command('say', permission=lambda s: s.is_superuser)
 async def say(session: CommandSession):
     await session.send(
         unescape(session.state.get('message') or session.current_arg))
