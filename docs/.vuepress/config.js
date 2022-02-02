4cc15f7afcad1d2f16bd80e9b998db2ab738d044
iyume <iyumelive@gmail.com>
2022-02-02 16:17:18 +0800

    doc: migrate vuepress to 1.x; apply nb-autodoc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,14 @@ module.exports = {
     markdown: {
         lineNumbers: true
     },
+    extendMarkdown(md) {
+        md.use(require('markdown-it-attrs'))
+    },
+    plugins: [
+        ['@vuepress/google-analytics', {
+            ga: 'UA-115509121-2'
+        }]
+    ],
     head: [
         ['link', { rel: 'icon', href: `/logo.png` }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
@@ -16,7 +24,6 @@ module.exports = {
         ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-150x150.png' }],
         ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }]
     ],
-    ga: 'UA-115509121-2',
     themeConfig: {
         repo: 'nonebot/nonebot',
         docsDir: 'docs',
@@ -29,7 +36,7 @@ module.exports = {
             { text: '主页', link: '/' },
             { text: '指南', link: '/guide/' },
             { text: '进阶', link: '/advanced/' },
-            { text: 'API', link: '/api.md' },
+            { text: 'API', link: '/api/' },
             { text: '术语表', link: '/glossary.md' },
             { text: '更新日志', link: '/changelog.md' },
             { text: '论坛', link: 'https://discussions.nonebot.dev/' },
@@ -81,6 +88,111 @@ module.exports = {
                     ]
                 }
             ],
+            '/api/': [
+                {
+                    title: 'API Reference',
+                    collapsable: false,
+                    sidebarDepth: 0,
+                    children: [
+                        {
+                            title: 'nonebot 模块',
+                            path: './'
+                        },
+                        {
+                            title: 'nonebot.argparse 模块',
+                            path: 'argparse'
+                        },
+                        {
+                            title: 'nonebot.default_config 模块',
+                            path: 'default_config'
+                        },
+                        {
+                            title: 'nonebot.exceptions 模块',
+                            path: 'exceptions'
+                        },
+                        {
+                            title: 'nonebot.helpers 模块',
+                            path: 'helpers'
+                        },
+                        {
+                            title: 'nonebot.log 模块',
+                            path: 'log'
+                        },
+                        {
+                            title: 'nonebot.message 模块',
+                            path: 'message'
+                        },
+                        {
+                            title: 'nonebot.natural_language 模块',
+                            path: 'natural_language'
+                        },
+                        {
+                            title: 'nonebot.notice_request 模块',
+                            path: 'notice_request'
+                        },
+                        {
+                            title: 'nonebot.permission 模块',
+                            path: 'permission'
+                        },
+                        {
+                            title: 'nonebot.plugin 模块',
+                            path: 'plugin'
+                        },
+                        {
+                            title: 'nonebot.sched 模块',
+                            path: 'sched'
+                        },
+                        {
+                            title: 'nonebot.session 模块',
+                            path: 'session'
+                        },
+                        {
+                            title: 'nonebot.typing 模块',
+                            path: 'typing'
+                        },
+                        {
+                            title: 'nonebot.command 模块',
+                            path: 'command/'
+                        },
+                        {
+                            title: 'nonebot.command.group 模块',
+                            path: 'command/group'
+                        },
+                        {
+                            title: 'nonebot.command.argfilter 模块',
+                            path: 'command/argfilter/'
+                        },
+                        {
+                            title: 'nonebot.command.argfilter.controllers 模块',
+                            path: 'command/argfilter/controllers'
+                        },
+                        {
+                            title: 'nonebot.command.argfilter.converters 模块',
+                            path: 'command/argfilter/converters'
+                        },
+                        {
+                            title: 'nonebot.command.argfilter.extractors 模块',
+                            path: 'command/argfilter/extractors'
+                        },
+                        {
+                            title: 'nonebot.command.argfilter.validators 模块',
+                            path: 'command/argfilter/validators'
+                        },
+                        {
+                            title: 'nonebot.experimental 模块',
+                            path: 'experimental/'
+                        },
+                        {
+                            title: 'nonebot.experimental.permission 模块',
+                            path: 'experimental/permission'
+                        },
+                        {
+                            title: 'nonebot.experimental.plugin 模块',
+                            path: 'experimental/plugin'
+                        },
+                    ]
+                }
+            ]
         },
     }
 }
